cc3c10ecd633cab80c72aaafdb84336c8cefcf69
Matteo D <alex3025game@gmail.com>
2020-09-05 11:48:50 +0200

    Update CreativeHandler.java

--- com/rgbcraft/baumod/main/handlers/CreativeHandler.java
+++ com/rgbcraft/baumod/main/handlers/CreativeHandler.java
@@ -1,5 +1,7 @@
 package com.rgbcraft.baumod.main.handlers;
 
+import com.rgbcraft.baumod.blocks.Blocks;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
@@ -10,9 +12,9 @@ import net.minecraftforge.oredict.OreDictionary;
 
 
 public class CreativeHandler extends CreativeTabs {
-	public static CreativeTabs ECCBlocks = new CreativeTab(CreativeTabs.getNextID(), "EyeCandyCraft Blocks");
-	public static CreativeTabs ECCDecorations = new CreativeTab(CreativeTabs.getNextID(), "EyeCandyCraft Decorations");
-	public static CreativeTabs ECCSigns = new CreativeTab(CreativeTabs.getNextID(), "EyeCandyCraft Signs");
+	public static CreativeTabs ECCBlocks = new CreativeTab(CreativeTabs.getNextID(), "EyeCandyCraft > Blocks");
+	public static CreativeTabs ECCDecorations = new CreativeTab(CreativeTabs.getNextID(), "EyeCandyCraft > Decorations");
+	public static CreativeTabs ECCSigns = new CreativeTab(CreativeTabs.getNextID(), "EyeCandyCraft > Signs");
 
 	private String name;
 
@@ -24,11 +26,12 @@ public class CreativeHandler extends CreativeTabs {
 	@SideOnly(Side.CLIENT)
 	public ItemStack getIconItemStack() {
 		if (this.name == ECCBlocks.getTabLabel()) {
-			return new ItemStack(Blocks.blockOsmoGen.blockID, 1, 0);
+			return new ItemStack(Blocks.timberframe.blockID, 1, 0);
 		} else if (this.name == ECCDecorations.getTabLabel()) {
-			return new ItemStack(LiquidUtils.items.get(LiquidUtils.items.size() - 1).itemID, 1, 0);
+			return new ItemStack(Blocks.ParabolaSat.blockID, 1, 0);
 		} else if (this.name == ECCSigns.getTabLabel()) {
-			
+			return new ItemStack(Items.d_Parkour.itemID, 1, 0);
+		}	
 			
 
 		return null;
